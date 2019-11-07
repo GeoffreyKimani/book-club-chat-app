5005bacc87d3562c0304b275317cdd1412974430
jeffkim <jeffkim207@gmail.com>
2019-11-07 13:15:35 +0300

    Partially refactored module; Book Club Registration.

--- ChatApp/Models/book.py
+++ ChatApp/Models/book.py
@@ -8,7 +8,7 @@ class Book(BaseModel.db.Model):
     author = AppDb.Column(AppDb.String(80), nullable=False, index=True)
     isbn = AppDb.Column(AppDb.String(40), nullable=False)
     category = AppDb.Column(AppDb.String(100), nullable=True)
-    review = AppDb.Column(AppDb.String(255), nullable=True)
+    review = AppDb.Column(AppDb.Text, nullable=True)
 
     def __repr__(self):
         return '<Book Title: {0} \nAuthor: {1} >'.format(self.title, self.author)
